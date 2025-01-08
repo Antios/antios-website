45679b82ce7d45afe083e0a5cb2dbff72a769229
Antios <micomeau@yahoo.com>
2025-01-08 15:02:36 -0400

    site stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
   site: "https://antios.github.io",
-  base: "/",
+  base: "/antios-website",
   trailingSlash: "always",
   integrations: [
     tailwind(
